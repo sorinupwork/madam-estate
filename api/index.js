80cd867f02dbe1024ab0cad9a103a573dd6e1df1
sorinupwork <sorin.upwork@gmail.com>
2023-11-22 07:03:21 +0200

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -17,7 +18,10 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
 
 app.listen(3000, () => {
   console.log(`Server listening on http://localhost:3000`);
