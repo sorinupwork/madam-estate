9733e7bf2fe7265a77c1098a7a348c4bfb2d155f
sorinupwork <sorin.upwork@gmail.com>
2023-11-22 07:17:48 +0200

    create middleware and a function to handle possible errors

--- api/index.js
+++ api/index.js
@@ -23,6 +23,16 @@ app.use(express.json());
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
+
 app.listen(3000, () => {
   console.log(`Server listening on http://localhost:3000`);
 });
