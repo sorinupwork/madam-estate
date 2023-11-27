3bdc38c2caf062d60e242a971c54dfa4d2cb2a87
sorinupwork <sorin.upwork@gmail.com>
2023-11-27 03:47:35 +0200

    create update listing api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -3,6 +3,7 @@ import express from 'express';
 import {
   createListing,
   deleteListing,
+  updateListing,
 } from '../controllers/listing.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
@@ -10,5 +11,6 @@ const router = express.Router();
 
 router.post('/create', verifyToken, createListing);
 router.delete('/delete/:id', verifyToken, deleteListing);
+router.post('/update/:id', verifyToken, updateListing);
 
 export default router;
