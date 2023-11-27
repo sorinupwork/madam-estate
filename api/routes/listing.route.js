29f0bd8ad5600d64118e6e6ce8fdf0f6a67909e4
sorinupwork <sorin.upwork@gmail.com>
2023-11-27 17:07:16 +0200

    create search api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -5,6 +5,7 @@ import {
   deleteListing,
   updateListing,
   getListing,
+  getListings,
 } from '../controllers/listing.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
@@ -14,5 +15,6 @@ router.post('/create', verifyToken, createListing);
 router.delete('/delete/:id', verifyToken, deleteListing);
 router.post('/update/:id', verifyToken, updateListing);
 router.get('/get/:id', getListing);
+router.get('/get', getListings);
 
 export default router;
